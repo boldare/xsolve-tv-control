f6ce627641340db69bc048bbb95aab8cd3d74d37
Tomasz Konieczny <tomasz.konieczny@xsolve.pl>
2018-02-08 16:03:20 +0100

    Code style changes

--- tv-cli.js
+++ tv-cli.js
@@ -4,7 +4,6 @@ const clu = require('command-line-usage');
 const fs = require('fs');
 var SimpleADB = require('simple-adb').SimpleADB;
 
-// var sadb = new SimpleADB();
 const config = JSON.parse(fs.readFileSync('tvconfig.json', 'utf8'));
 const appConfig = JSON.parse(fs.readFileSync('appconfig.json', 'utf8'));
 
@@ -111,14 +110,12 @@ if(cliOptions.poweron || cliOptions.poweroff) {
 }
 
 if(cliOptions.run) {
-    console.log(cliOptions.run);
     return runApplication(tvList, cliOptions.run).then(function() {
         process.exit(0);
     });
 }
 
 if(cliOptions.yt) {
-    console.log(cliOptions.yt);
     return runYoutubeMovie(tvList, cliOptions.yt).then(function() {
         process.exit(0);
     });
@@ -127,8 +124,6 @@ if(cliOptions.yt) {
 //=======methods
 
 function powerSet(tvList, powerState) {
-    console.log(`powerSetAll TV to state: ${ powerState }`);
-
     return new Promise(function(resolve, reject){
         let elementsNumber = tvList.length;
         let promiseArray = [];
@@ -153,10 +148,7 @@ function powerSet(tvList, powerState) {
 }
 
 async function runApplication(tvList, applicationName) {
-    console.log(`runApplication`);
-
     var applicationPackageName = appConfig[applicationName] ? appConfig[applicationName] : applicationName;
-
     let elementsNumber = tvList.length;
 
     for (i = 0; i < elementsNumber; i++) {
@@ -165,8 +157,6 @@ async function runApplication(tvList, applicationName) {
 }
 
 async function runYoutubeMovie(tvList, url) {
-    console.log(`runYoutubeMovie`);
-
     let elementsNumber = tvList.length;
 
     for (i = 0; i < elementsNumber; i++) {
@@ -178,6 +168,7 @@ async function runYoutubeMovie(tvList, url) {
 
 function ll_powerSet(tvName, powerState) {
     console.log(`powerSet TV ${ tvName } to state: ${ powerState }`);
+
     var tvConfig = config[tvName];
 
     return new BraviaRemoteControl(tvConfig.ip, 80, tvConfig.key).sendAction(powerState ? 'PowerOn' : 'PowerOff');
@@ -189,20 +180,22 @@ function ll_runApplication(tvName, applicationPackageName) {
 
     console.log(`running "${ applicationPackageName }" on "${ tvName }" TV (${ ipAddress }).`);
 
-    return sadb.connect(ipAddress).then(function() {
-        return sadb.execAdbShellCommand(`monkey -p ${ applicationPackageName } -c android.intent.category.LAUNCHER 1`)
-    })
-    .catch(e => {
-        console.log('ll_runApplication failed - retrying');
-        return ll_runApplication(tvName, applicationPackageName);
-    });
+    return sadb
+        .connect(ipAddress)
+        .then(function() {
+            return sadb.execAdbShellCommand(`monkey -p ${ applicationPackageName } -c android.intent.category.LAUNCHER 1`)
+        })
+        .catch(e => {
+            console.log('ll_runApplication failed - retrying');
+            return ll_runApplication(tvName, applicationPackageName);
+        });
 }
 
 function ll_runYoutubeMovie(tvName, url) {
     var sadb = new SimpleADB();
     var ipAddress = config[tvName].ip;
 
-    console.log(`running youtube "${ url }" on "${ tvName }" TV (${ ipAddress }).`);
+    console.log(`Running Youtube url "${ url }" on "${ tvName }" TV (${ ipAddress }).`);
 
     return sadb
         .connect(ipAddress)
