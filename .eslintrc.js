5438c554db412ef4f0bf26ca72cbd4451c0cf502
Tomasz Konieczny <tomasz.konieczny@xsolve.pl>
2018-02-09 13:15:06 +0100

    eslint config, package.json updated

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "extends": ["eslint:recommended", "google"],
+    "parserOptions": {
+        "sourceType": "module",
+        "ecmaVersion": 2017
+    },
+    "rules": {
+
+    }
+};
