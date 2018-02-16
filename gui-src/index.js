0bc1059a05f954fe2d9e5cfebad66320d353eec4
Tomasz Konieczny <tomasz.konieczny@xsolve.pl>
2018-02-16 13:44:44 +0100

    getSelectedDevices, test youtube dialog

--- gui-src/index.js
+++ gui-src/index.js
@@ -14,11 +14,11 @@ function getData() {
         var powerColorClass = getTvPowerState(name) ? 'power-on': 'power-off';
 
         tvListElem.innerHTML += `
-        <div class="card text-center text-wrap ${powerColorClass}">
+        <div class="card text-center text-wrap ${powerColorClass}" tv="${ name }">
             <div class="form-check">
-                <input class="form-check-input position-static checkbox-big" type="checkbox" id="blankCheckbox" value="option1" aria-label="...">
+                <input class="form-check-input position-static checkbox-big" type="checkbox" value="${ name }">
             </div>
-            <div class="card-body col align-self-end" tv="${ name }">
+            <div class="card-body col align-self-end">
                     <h4 class="card-title" id="name">${ name }</h4>
                     <p class="card-text">${ tvlistJson[name].ip } <br /> ${ tvlistJson[name].mac }</p>
             </div>
@@ -33,13 +33,32 @@ getData();
 //
 // }
 // document.querySelector('#checkbox-select-all').addEventListener('click', selectAllCheckboxes)
+getSelectedDevices()
+
+function getSelectedDevices() {
+    var array = []
+    var selectedCheckboxes = document.querySelectorAll('input[type=checkbox]:checked');
+
+    for (var i = 0; i < selectedCheckboxes.length; i++) {
+      array.push(selectedCheckboxes[i].value)
+    }
+
+    return array;
+}
 
 function powerOn() {
-    return actions.powerSet(['Aula 3'], true);
+    return actions.powerSet(getSelectedDevices(), true);
 }
 
 function powerOff() {
-    return actions.powerSet(['Aula 3'], false);
+    return actions.powerSet(getSelectedDevices(), false);
+}
+
+function runYoutubeDialog() {
+    let win = new BrowserWindow({ parent: require('electron').remote.getCurrentWindow(), modal: true, frame: false, width: 400, height: 200});
+    win.on('close', function() { win = null });
+    win.loadURL(path.join('file://', __dirname, 'youtube.html'));
+    win.show();
 }
 
 //test only
