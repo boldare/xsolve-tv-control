3664728f13291a9e63f3c763e9a117e4b21b5c51
Tomasz Konieczny <tomasz.konieczny@xsolve.pl>
2018-02-21 14:24:58 +0100

    Electron - DevTools disabled

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ function createWindow() {
     }));
 
     // win.setFullScreen(true);
-    win.webContents.openDevTools();
+    // win.webContents.openDevTools();
 
     win.on('closed', () => {
         win = null;
